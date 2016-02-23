482ef6aa270d4d27b9b74c8c21172a041786915d
Chang Kon Han <chang_kon@hotmail.com>
2016-02-23 23:50:09 +1300

    added template resource and rest application

---
+++ src/main/java/blaze/athena/application/AthenaRestApplication.java
@@ -0,0 +1,30 @@
+package blaze.athena.application;
+
+import blaze.athena.services.HelloResource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * <p>Entry point into REST service for Athena</p>
+ * @author Chang Kon Han
+ * @author John Law
+ * @author Wesley Yep
+ * @since 23 Feb 2016
+ */
+@ApplicationPath("/services")
+public class AthenaRestApplication extends Application {
+
+    private Set<Class<?>> components = new HashSet<Class<?>>();
+
+    public AthenaRestApplication() {
+        components.add(HelloResource.class);
+    }
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        return components;
+    }
+}
