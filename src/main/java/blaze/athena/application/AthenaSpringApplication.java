50447aa8962fd3d4f67e426aa49f1ea90b167eb3
Chang Kon Han <chang_kon@hotmail.com>
2016-02-29 21:36:06 +1300

    added greetingcontroller. integrate spring with resteasy. comment out config package, for now

---
+++ src/main/java/blaze/athena/application/AthenaSpringApplication.java
@@ -0,0 +1,55 @@
+package blaze.athena.application;
+
+import org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher;
+import org.jboss.resteasy.plugins.server.servlet.ResteasyBootstrap;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ServletContextInitializer;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.servlet.ServletContextListener;
+
+/**
+ * <p>Entry point into Spring boot application</p>
+ * @author Chang Kon Han
+ * @author John Law
+ * @author Wesley Yep
+ * @since 29 Feb 2016
+ */
+@SpringBootApplication
+@ComponentScan({"blaze.athena.controller"})
+@EnableAutoConfiguration
+public class AthenaSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AthenaSpringApplication.class, args);
+    }
+
+    @Bean
+    public ServletContextInitializer initializer() {
+        return servletContext -> {
+            // Resteasy configuration
+            servletContext.setInitParameter("resteasy.scan", "true");
+            servletContext.setInitParameter("resteasy.servlet.mapping.prefix", "/services");
+        };
+    }
+
+    @Bean
+    public ServletContextListener restEasyBootstrap() {
+        return new ResteasyBootstrap();
+    }
+
+    @Bean
+    public ServletRegistrationBean restEasyServlet() {
+        final ServletRegistrationBean registrationBean = new ServletRegistrationBean();
+        registrationBean.setServlet(new HttpServletDispatcher());
+        registrationBean.setName("athena-resteasy");
+        registrationBean.addUrlMappings("/services/*");
+        registrationBean.addInitParameter("javax.ws.rs.Application", "blaze.athena.application.AthenaRestApplication");
+        return registrationBean;
+    }
+
+}
