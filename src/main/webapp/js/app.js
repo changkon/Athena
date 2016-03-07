4b0c6b462510c994d09a1cac563435b11ccc9352
Chang Kon Han <chang_kon@hotmail.com>
2016-03-07 21:50:23 +1300

    added angular bootstrap files

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,6 +1,6 @@
 // Athena angularjs app
 // loads config and routing
-angular.module('athena', ['ui.router','ngResource'])
+angular.module('athena', ['ui.router','ngResource','ui.bootstrap'])
 .config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
         .state('dashboard', {
