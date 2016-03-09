9b06ac14fa62bcbfa7e3ec01930519e7507b65f6
Chang Kon Han <chang_kon@hotmail.com>
2016-03-10 01:12:00 +1300

    added pagination. questions not styled yet

--- src/main/webapp/js/controllers/dashboard/dashboardQuestionCtrl.js
+++ src/main/webapp/js/controllers/dashboard/dashboardQuestionCtrl.js
@@ -4,6 +4,31 @@
     app.controller('DashboardQuestionCtrl', ['$scope','FilePost', '$timeout', function($scope, FilePost, $timeout) {
         $scope.myFile = null;
         $scope.fileName = "Your PDF File";
+        $scope.questions = null;
+
+        $scope.question = {};
+
+        // query pagination settings
+        $scope.pag = {
+            currentPage: 1,
+            maxSize: 10,
+            length: 1
+        };
+
+        $scope.update = function(questionNumber) {
+            var question = $scope.questions.body[questionNumber];
+            $scope.question.answer = question.answer;
+            $scope.question.answers = question.answers;
+            $scope.question.question = question.question;
+        };
+
+        $scope.checkAnswer = function(answerNumber) {
+            if (answerNumber == $scope.question.answer) {
+                console.log('correct answer');
+            } else {
+                console.log('incorrect answer');
+            }
+        };
 
         $scope.uploadFile = function(){
             var file = $scope.myFile;
@@ -13,6 +38,14 @@
             fd.append('uploadedFile', file);
 
             FilePost.create({}, fd).$promise.then(function(res){
+                $scope.questions = res;
+
+                $scope.question.answer = res.body[0].answer;
+                $scope.question.answers = res.body[0].answers;
+                $scope.question.question = res.body[0].question;
+
+                $scope.pag.numfound = res.body.length;
+                $scope.pag.currentPage = 1;
                 console.log(res);
             }).catch(function (err) {
                 console.log("error: ");
