36248eb563bba7de6ed139be023edfa4b3c8e937
Chang Kon Han <chang_kon@hotmail.com>
2016-04-07 22:14:27 +1200

    shows progress from results. but doesn't update status label

--- src/main/webapp/js/controllers/dashboard/dashboardQuestionCtrl.js
+++ src/main/webapp/js/controllers/dashboard/dashboardQuestionCtrl.js
@@ -1,6 +1,14 @@
 (function() {
     var app = angular.module('athena');
 
+    // custom filter for progressPag
+    app.filter('progressRes', function() {
+        return function(input, arr, pageNumber, itemsPerPage) {
+            var startIndex = (pageNumber - 1)*itemsPerPage;
+            return arr.slice(startIndex, startIndex+itemsPerPage);
+        }
+    });
+
     app.controller('DashboardQuestionCtrl', ['$scope','FilePost', '$timeout', function($scope, FilePost, $timeout) {
         $scope.showQuestions = false;
         $scope.myFile = { result : null };
@@ -14,6 +22,13 @@
         $scope.pag = {
             currentPage: 1,
             maxSize: 10,
+            length: 1
+        };
+
+        $scope.progressPag = {
+            currentPage: 1,
+            maxSize: 5,
+            itemsPerPage: 5,
             length: 1
         };
 
@@ -32,15 +47,22 @@
             setFontSize($scope.question.questions);
         };
 
+//        $scope.progressUpdate = function(pageNumber) {
+//            var startIndex = $scope.progressPag.maxSize * pageNumber - $scope.progressPag.maxSize;
+//            $scope.progressPag.res = $scope.questions.body.slice(startIndex, startIndex + $scope.progressPag.maxSize);
+//        };
+
         $scope.checkAnswer = function(answerNumber) {
             if (!$scope.answerOnce) {
                 var correctElement = angular.element( document.querySelector( '#answer-' + $scope.question.answer ) );
                 if (answerNumber == $scope.question.answer) {
                     console.log('correct answer');
+                    $scope.question.gotCorrect = true;
                 } else {
                     console.log('incorrect answer');
                     var incorrectElement = angular.element( document.querySelector( '#answer-' + answerNumber ) );
                     incorrectElement.addClass('incorrect');
+                    $scope.question.gotCorrect = false;
                 }
                 correctElement.addClass('correct');
                 $scope.answerOnce = true;
@@ -66,17 +88,27 @@
                 fd.append('uploadedText', text.replace("’", "'"));
             }
             $scope.myPromise = FilePost.create({}, fd).$promise.then(function(res){
+                // update returned object to store gotCorrect
+                for (var i = 0; i < res.body.length; i++) {
+                    res.body[i].correctAnswer = null;
+                    res.body[i].questions = res.body[i].question.split(/\\n|:\\n/g);
+                }
                 $scope.questions = res;
 
-                $scope.question.answer = res.body[0].answer;
-                $scope.question.answers = res.body[0].answers;
-                $scope.question.questions = res.body[0].question.split(/\\n|:\\n/g);
-                $scope.question.topic = res.body[0].topic;
+                $scope.question = $scope.questions.body[0];
+//                $scope.question.answer = res.body[0].answer;
+//                $scope.question.answers = res.body[0].answers;
+//                $scope.question.questions = res.body[0].question.split(/\\n|:\\n/g);
+//                $scope.question.topic = res.body[0].topic;
                 setFontSize($scope.question.questions);
 
+                // set pagination
                 $scope.pag.numfound = res.body.length;
                 $scope.pag.currentPage = 1;
-                console.log(res);
+
+                $scope.progressPag.numfound = res.body.length;
+                $scope.progressPag.currentPage = 1;
+
                 $scope.showQuestions = true;
             }).catch(function (err) {
                 console.log("error: ");
