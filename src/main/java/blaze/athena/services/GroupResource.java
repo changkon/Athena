c65ace38df7792599ee5b98aa29bccf458038911
Wesley Yep <wesleyyep@hotmail.com>
2016-04-18 10:19:08 +1200

    membership

--- src/main/java/blaze/athena/services/GroupResource.java
+++ src/main/java/blaze/athena/services/GroupResource.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author Chang Kon Han
  * @author John Law
@@ -25,8 +28,10 @@ public class GroupResource implements IGroupResource {
 
     @Override
     public ResponseEntity getGroups(int ownerId) {
-        SelectGroupsByOwnerQuery query = new SelectGroupsByOwnerQuery();
-        return new ResponseEntity<>(query.select(ownerId), HttpStatus.OK);
+        SelectGroups query = new SelectGroups();
+        Set<List<String>> groups = query.selectByOwner(ownerId);
+        groups.addAll(query.selectByMember(ownerId));
+        return new ResponseEntity<>(groups, HttpStatus.OK);
     }
 
 }
