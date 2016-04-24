f70730ca0fc8f74cd0b2ebf0fc72d6ecc1edb993
Wesley Yep <wesleyyep@hotmail.com>
2016-04-25 10:36:42 +1200

    working on retrieval

--- src/main/webapp/js/controllers/dashboard/dashboardSearchCtrl.js
+++ src/main/webapp/js/controllers/dashboard/dashboardSearchCtrl.js
@@ -1,7 +1,29 @@
 (function() {
     var app = angular.module('athena');
 
-    app.controller('DashboardSearchCtrl', ['$scope', function($scope) {
+    app.controller('DashboardSearchCtrl', ['$scope', 'GetCategories', function($scope, GetCategories) {
+        $scope.categories = null;
+
         console.log("made it!");
+
+        refreshCategories();
+
+       function refreshCategories() {
+            if ($scope.categories != null) {
+
+            } else {
+                $scope.myPromise = GetCategories.create().$promise.then(function(res){
+                    var categories = [];
+                    for (i = 0; i < res.body.length; i++) {
+                        categories.push({ text: res.body[i]})
+                    }
+                    $scope.categories = categories;
+                    //console.log(categories);
+                    $scope.categories = categories;
+                });
+                return $scope.myPromise;
+            }
+        };
+
     }]);
 })();
